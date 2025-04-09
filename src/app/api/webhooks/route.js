eedaeb0793b18554fb1e58e9fcf4461ee8ea3238
westrincodes <westrinwaithaka04@gmail.com>
2025-04-09 18:23:30 +0300

    add mongodb and use it to save data from clerk

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,5 +1,6 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
+import { createOrUpdateUser, deleteUser } from '@/lib/actions/user'
 
 export async function POST(req) {
   const SIGNING_SECRET = process.env.SIGNING_SECRET
@@ -46,16 +47,46 @@ export async function POST(req) {
 
   // Do something with payload
   // For this guide, log payload to console
-  const { id } = evt.data
-  const eventType = evt.type
+  const { id } = evt?.data
+  const eventType = evt?.type
   console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
   console.log('Webhook payload:', body)
 
-  if (eventType === 'user.created'){
-    console.log('User created'); 
+  if (eventType === 'user.created' || eventType === 'user.updated'){
+    const { id, first_name, last_name, image_url, email_addresses, username } = evt?.data;
+    try {
+      await createOrUpdateUser(
+        id,
+        first_name,
+        last_name,
+        image_url,
+        email_addresses,
+        username
+      );
+      return new Response('User is created or updated', {
+        status: 200,
+      });
+    } catch (error) {
+      console.log('Error creating or updating user:', error);
+      return new Response('Error occured', {
+        status: 400,
+      });
+    }
+  
   }
   if (eventType === 'user.updated'){
-    console.log('User updated'); 
+    const { id } = evt?.data;
+    try {
+      await deleteUser(id);
+      return new Response('User is deleted', {
+        status: 200,
+      });
+    } catch (error) {
+      console.log('Error deleting user:', error);
+      return new Response('Error occured', {
+        status: 400,
+      });
+    }
   }
 
   return new Response('Webhook received', { status: 200 })
---
+++ src/lib/models/user.mode.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+
+const userSchema = new mongoose.schema({
+    cerkId: {
+        type:String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type:String,
+        required: true,
+    },
+    firstName: {
+        type:String,
+        required: true,
+    },
+    lastName: {
+        type:String,
+        required: true,
+    },
+    userName: {
+        type:String,
+        required: true,
+    },
+    avatar: {
+        type:String,
+        required: true,
+    }
+},{timestamps:true});
+
+const User = mongoose.models.User || mongoose.model('user',userSchema);
+
+export default User;
