ea2eb4f0334a9f38dd1ea23439f62bbe1bad1628
westrincodes <westrinwaithaka04@gmail.com>
2025-04-09 18:37:44 +0300

    corrected schema

--- src/lib/models/user.mode.js
+++ src/lib/models/user.mode.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 
-const userSchema = new mongoose.schema({
+const userSchema = new mongoose.Schema({
     cerkId: {
         type:String,
         required: true,
